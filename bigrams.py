88b89c4bd6e3d8cfb305afbcad3ba9481f00b347
Rowan Shah <rowanshah@gmail.com>
2020-01-04 20:46:54 +0000

    Create bigrams.py

---
+++ bigrams.py
@@ -0,0 +1,122 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import itertools
+import collections
+
+import tweepy as tw
+import nltk
+from nltk import bigrams
+from nltk.corpus import stopwords
+import re
+import networkx as nx
+
+import warnings
+warnings.filterwarnings("ignore")
+
+sns.set(font_scale=1.5)
+sns.set_style("whitegrid")
+
+consumer_key = 'to9SXQQjhDSOdptpZa5wNKpSO'
+consumer_secret = 'u8XMH5OYc5oQh3igbBO59ZOjQRj9uekUiVNUgdTiTnSewsEdYP'
+access_token = '1205807190361153537-E09RCWOOBynFVofLw61ZfkAun2EgSx'
+access_token_secret = 'aunh6iie4TKVrrcyB2OCCgY1erO1T02z78RM7ZQjMAmzG'
+
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+
+# Create a custom search term and define the number of tweets
+search_term = "#climate+change -filter:retweets"
+
+tweets = tw.Cursor(api.search,
+                   q=search_term,
+                   lang="en",
+                   since='2018-11-01').items(1000)
+
+def remove_url(txt):
+    return " ".join(re.sub("([^0-9A-Za-z \t])|(\w+:\/\/\S+)", "", txt).split())
+
+    # Remove URLs
+tweets_no_urls = [remove_url(tweet.text) for tweet in tweets]
+
+# Create a sublist of lower case words for each tweet
+words_in_tweet = [tweet.lower().split() for tweet in tweets_no_urls]
+
+# Download stopwords
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+
+# Remove stop words from each tweet list of words
+tweets_nsw = [[word for word in tweet_words if not word in stop_words]
+              for tweet_words in words_in_tweet]
+
+# Remove collection words
+collection_words = ['climatechange', 'climate', 'change']
+
+tweets_nsw_nc = [[w for w in word if not w in collection_words]
+                 for word in tweets_nsw]
+
+
+# Create list of lists containing bigrams in tweets
+terms_bigram = [list(bigrams(tweet)) for tweet in tweets_nsw_nc]
+
+# View bigrams for the first tweet
+terms_bigram[0]
+
+# Original tweet without URLs
+tweets_no_urls[0]
+
+# Clean tweet 
+tweets_nsw_nc[0]
+
+# Flatten list of bigrams in clean tweets
+bigrams = list(itertools.chain(*terms_bigram))
+
+# Create counter of words in clean bigrams
+bigram_counts = collections.Counter(bigrams)
+
+bigram_counts.most_common(20)
+
+bigram_df = pd.DataFrame(bigram_counts.most_common(20),
+                             columns=['bigram', 'count'])
+
+bigram_df
+
+# Create dictionary of bigrams and their counts
+d = bigram_df.set_index('bigram').T.to_dict('records')
+
+# Create network plot 
+G = nx.Graph()
+
+# Create connections between nodes
+for k, v in d[0].items():
+    G.add_edge(k[0], k[1], weight=(v * 10))
+
+G.add_node("Win", weight=100)
+
+fig, ax = plt.subplots(figsize=(10, 8))
+
+pos = nx.spring_layout(G, k=1)
+
+# Plot networks
+nx.draw_networkx(G, pos,
+                 font_size=16,
+                 width=3,
+                 edge_color='grey',
+                 node_color='purple',
+                 with_labels = False,
+                 ax=ax)
+
+# Create offset labels
+for key, value in pos.items():
+    x, y = value[0]+.135, value[1]+.045
+    ax.text(x, y,
+            s=key,
+            bbox=dict(facecolor='red', alpha=0.25),
+            horizontalalignment='center', fontsize=13)
+
+
+
+                 
